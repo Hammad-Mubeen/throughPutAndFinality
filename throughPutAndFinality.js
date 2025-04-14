a374a742cb472e5ac3058d6517ad16cd2ec8b68e
Muhammad Hammad Mubeen <twoface03205622887@gmail.com>
2025-04-14 22:57:41 +0500

    intervals

--- throughPutAndFinality.js
+++ throughPutAndFinality.js
@@ -1,6 +1,7 @@
 import { providers, Wallet, utils } from "ethers";
 
 const RPC_URL = `http://3.15.178.121:8020/rpc/ethrpc`;
+//const RPC_URL = 'https://3b6f-2407-d000-a-60b6-4cc7-6f90-bcf0-2b9d.ngrok-free.app/rpc/ethrpc';
 const PRIVATE_KEY = `0x8610452e57d659fdd68298d5b7da65ad6ecba04724158043f9b77f9e54b47517`;
 const RECEIVER = `0x3173E63d2Abbc1582fE41719EDeEE25A2624aC9D`;
 
@@ -11,10 +12,15 @@ const MIN_BALANCE = utils.parseEther("0.05");
 const FUND_AMOUNT = utils.parseEther("100");
 const gasPrice = utils.parseUnits("40", "gwei");
 const gasLimit = 21000;
-const frequency = 60_000 * 15; // 15 minutes
-let nonce = 0;
+const frequency = 1000 * 1; // 15 minutes
+let nonce = 213;
 
-let wallets = [];
+let wallets = [{
+  address: "0x5D421D63551427A073b4d940d96B13018ca107A4",
+  pk: PRIVATE_KEY
+}];
+
+//let wallets = [];
 let txHashes = [];
 
 async function createWallets()
@@ -35,11 +41,12 @@ const truncate = (address) => {
 const checkAndFundWallets = async (provider) => {
   try {
     const balance = await provider.getBalance(wallets[0].address);
+    console.log(balance);
     if (balance.lt(MIN_BALANCE)) {
       console.log(
         `Funding wallet ${wallets[0].address} with ${utils.formatEther(FUND_AMOUNT)} ETH`
       );
-      await sendETH(wallet, wallets[0].address, FUND_AMOUNT, provider);
+      //await sendETH(wallet, wallets[0].address, FUND_AMOUNT, provider);
     }
     else{
       console.log(
@@ -75,11 +82,11 @@ const sendETH = async (senderWallet, to, amount, provider) => {
 
 
 const sendETHFromAllWallets = async (provider) => {
-  const value = utils.parseEther("0.1");
-  for (var i = 0; i < 100; i++) {
+  const value = utils.parseEther("0.001");
+  for (var i = 0; i < 5; i++) {
     try {
       const userWallet = new Wallet(wallets[0].pk, provider);
-      console.log("nonce: ",nonce);
+      //let nonce = await provider.getTransactionCount(wallets[0].address);
       const tx = {
       to: RECEIVER,
       nonce, 
@@ -90,6 +97,10 @@ const sendETHFromAllWallets = async (provider) => {
       };
 
       const signedTx = await userWallet.signTransaction(tx);
+      // let promise = await provider.send("eth_sendRawTransaction", [signedTx]);
+      // console.log("result hash: ",promise);
+      // let receipt = await provider.send("eth_getTransactionReceipt", [promise]);
+      // console.log("\n receipt txHash : " + JSON.stringify(receipt));
       let promise = provider.send("eth_sendRawTransaction", [signedTx]);
       txHashes.push(promise);
       nonce = nonce + 1;
@@ -97,20 +108,26 @@ const sendETHFromAllWallets = async (provider) => {
       console.log(`Failed to send ETH from ${truncate(wallets[0].address)} wallet`,error);
     }
   }
-  console.log("Fire all transactions at once: ",txHashes);
+  console.log("Fire all transactions at once ...");
   // Fire all transactions at once
   const results = await Promise.allSettled(txHashes);
   console.log("All transactions fired at once... ✅");
 
   console.log("Checking all fired transactions responses: ");
-  results.forEach((res, i) => {
+  results.forEach(async(res, i) => {
     if (res.status === "fulfilled") {
       console.log(`TX ${i}: ✅ Sent! Hash: ${res.value}`);
+      //let receipt = await provider.send("eth_getTransactionReceipt", [res.value]);
+      //receipt = JSON.stringify(receipt);
+      //console.log("\n receipt txHash : " + receipt);
     } else {
-      console.log(`TX ${i}: ❌ Failed - ${res}`);
+      console.log(`TX ${i}: ❌ Failed - ${res.reason}`);
     }
   });
   txHashes = [];
+  console.log("nonce: ",nonce);
+  sendETHFromAllWallets(provider);
+  return;
 };
 
 const checkSendETHFromAllWalletsTXs = async (provider) => {
@@ -139,12 +156,13 @@ const createWalletHelper = async () => {
 const main = async () => {
   try {
     console.log("Interval Process Started: ");
-    await checkAndFundWallets(provider);
+    //await checkAndFundWallets(provider);
     await sendETHFromAllWallets(provider);
   } catch (error) {
     console.log("Main function error: ",error);
   }
 };
 
-createWalletHelper();
-setInterval(main, frequency);
+//createWalletHelper();
+main();
+//setInterval(main, frequency);
