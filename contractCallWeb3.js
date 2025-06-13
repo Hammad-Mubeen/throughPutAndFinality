dd863250292539a1ae1a1ef5adbce2019d00c0f6
Muhammad Hammad Mubeen <twoface03205622887@gmail.com>
2025-06-14 01:22:37 +0500

    nonce increased

--- contractCallWeb3.js
+++ contractCallWeb3.js
@@ -1,16 +1,12 @@
 import {Web3} from 'web3';
 
-let RPC_URLs = [
-`http://18.190.161.56:8020/rpc/ethrpc`,
-`http://18.190.161.56:8030/rpc/ethrpc`,
-`http://18.190.161.56:8040/rpc/ethrpc`,
-`http://18.190.161.56:8050/rpc/ethrpc`];
+let RPC_URLs = [`http://18.190.161.56:8030/rpc/ethrpc`];
 
 let index = 0;
 let CONTRACT_ADDRESS = "0x67F1a9F8b4f40015D47Fc296Df9aFC3E7f9B4c3d";
 let AMOUNT_TO_REWARD = 100; // Each reward call mints this much
 let gas = 50000;
-let nonce = 550;
+let nonce = 623;
 let TPS = 35, whenToChangePort = TPS/2;
 let txHashes= [];
 
@@ -685,22 +681,22 @@ async function callRewardNTimes() {
   console.log("All transactions fired at once... ✅");
 
   console.log("Checking all fired transactions responses: ");
-  let flag = 0;
+  //let flag = 0;
   results.forEach(async(res, i) => {
     if (res.status === "fulfilled") {
       console.log("TX " + i + ": ✅ Sent! Hash: " + safeStringify(res.value.transactionHash));
     } else {
       console.log("TX " + i + ": ❌ Failed - " +safeStringify(res.reason));
-      flag++;
+      //flag++;
     }
   });
 
-  if(flag >= whenToChangePort)
-  {
-    await changeRPC();
-    web3 = new Web3(RPC_URLs[index]);
-    contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
-  }
+//   if(flag >= whenToChangePort)
+//   {
+//     await changeRPC();
+//     web3 = new Web3(RPC_URLs[index]);
+//     contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
+//   }
 
   txHashes = [];
   nonce = nonce + 1;
